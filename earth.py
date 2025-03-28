85bc864ccd747fbe43f9b3d8bc959d76fac3d555
paulinacinque <pcinque1@mail.austral.edu.ar>
2025-03-28 12:54:53 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    x_menor = x < y
+    y_menor = y < x
+
+    print(f"The result of {x} comes first in the dictionary than {y} is {x_menor}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {y_menor}.")
